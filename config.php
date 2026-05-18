4b290fc2f0cd39fad2d4b0d0cd4edd6769d55fa8
Adam <gajelastapi@gmail.com>
2026-05-18 07:14:11 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+const DB_HOST = '127.0.0.1';
+const DB_PORT = 3306;
+const DB_NAME = 'absensi_smk';
+const DB_USER = 'root';
+const DB_PASS = '';
+
+const EXCEL_PATH = __DIR__ . '/../Peserta UTS Gsl X (1).xlsx';
+const PROFILE_EXCEL_PATH = __DIR__ . '/../data siswa foto/DATA KLS X 2025.xlsx';
+const ADMIN_USERNAME = 'admin';
+const ADMIN_PASSWORD = 'smkn2guru';
